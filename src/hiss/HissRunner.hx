3b31f785f976ab3d4ad9329c3121ba887a2b7094
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-05-09 16:38:20 -0600

    Can scope in on a dictionary now

--- src/hiss/HissRunner.hx
+++ src/hiss/HissRunner.hx
@@ -16,28 +16,13 @@ class HissRunner {
     var interp: HissInterp;
     var reader: HissReader;
 
-    /** A Hiss list of HValues representing unevaluated expressions in the program being run. May be modified at runtime, i.e. by a (load) call. **/
+    /** A Hiss list of HValues representing unevaluated instructions in the program being run. May be modified at runtime, i.e. by a (load) call. **/
     var program: HValue;
     
     // TODO this HissRunner will also need to keep a structure of every intermediate expression that hasn't been evaluated yet! i.e. in (concat "Hey" "you" "rock") all of the strings will need to be stored until the funcall completes. 
     
-    /** A Hiss int representing the index of the next expression to evaluate. These do not correspond to "statements" in a traditional programming language sense. Here are some examples of program counter indexing:
-
-        "Hello world"
-        0
-        -------------
-        ( print "Hello world")
-        0 1     2
-        -------------
-        '( print "Hello world")
-        0
-        -------------
-        ( setq str "Hello world")
-        0 1    2   3
-        `(print ,str)
-        4       5
-    **/
-    var programCounter: HValue;
+    /** A Hiss int representing the index of the next instruction to evaluate. Nested expressions are not counted. **/
+    var instructionPointer: HValue;
 
     public function new() {
 
