6f4e9d26f5ad24ad58502420f8a38735187b054a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-05-17 16:33:14 -0600

    Removing HissInterp.load()

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,9 +1,12 @@
 package test;
 
+import hiss.StaticFiles;
+
 class TestAll {
   public static function main() {
+    StaticFiles.compileWith("test-std.hiss");
     utest.UTest.run([
-      new test.HissTestCase("test/test-std.hiss"),
+      new test.HissTestCase("test-std.hiss"),
       new test.HStreamTestCase(),
       new test.HissReaderTestCase(),
       //new test.NativeObjectTestCase(),
