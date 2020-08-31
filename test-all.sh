e54a02a79bcad1cf487557d10ad914d9dbc166fb
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-08-31 11:50:49 -0600

    Add c++ target

--- test-all.sh
+++ test-all.sh
@@ -4,3 +4,4 @@ haxe build-scripts/test/test-py.hxml
 haxe build-scripts/test/test-interp.hxml
 haxe build-scripts/test/test-js.hxml
 haxe build-scripts/test/test-nodejs.hxml
+haxe build-scripts/test/test-cpp.hxml
