4c9908a5b970d1e76ef6ca0d5f330ca738da3323
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-06 01:52:01 -0600

    install hxcpp for travis

--- test-travis.sh
+++ test-travis.sh
@@ -5,6 +5,7 @@ haxelib install haxe-strings
 haxelib install uuid
 haxelib install utest
 haxelib install hxnodejs
+haxelib install hxcpp
 haxe build-scripts/test/test-interp.hxml && \
 haxe build-scripts/test/test-py3.hxml && \
 haxe build-scripts/test/test-js.hxml && \
