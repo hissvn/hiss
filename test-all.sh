202541ed0cce90a7a5a8dfdb9d4154fd56985b7a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-05 20:19:35 -0600

    Install libs before build

--- test-all.sh
+++ test-all.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+haxelib install ihx
+haxelib install haxe-strings
+haxelib install uuid
+haxelib install utest
 haxe build-scripts/test/test-interp.hxml && \
 haxe build-scripts/test/test-py.hxml && \
 haxe build-scripts/test/test-js.hxml && \
