7c74ea999b6ee28100f18a751567fc82e2d16eba
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-10 15:58:25 -0600

    Most efficient test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -6,13 +6,15 @@ if [ ! -z "$1" ]; then
     end="&& \\"
 fi
 
-# Build repls for each target that has one, so the tests can pipe input to them
-eval "haxe build-scripts/repl/build-cpp-repl.hxml $end
-haxe build-scripts/repl/build-nodejs-repl.hxml $end
-haxe build-scripts/repl/build-py-repl.hxml $end
+eval "haxe build-scripts/test/test-interp.hxml $end
 
-haxe build-scripts/test/test-interp.hxml $end
-haxe build-scripts/test/test-py.hxml $end
 haxe build-scripts/test/test-js.hxml $end
+
+haxe build-scripts/repl/build-cpp-repl.hxml && \
+haxe build-scripts/test/test-cpp.hxml $end
+
+haxe build-scripts/repl/build-nodejs-repl.hxml && \
 haxe build-scripts/test/test-nodejs.hxml $end
-haxe build-scripts/test/test-cpp.hxml"
+
+haxe build-scripts/repl/build-py-repl.hxml && \
+haxe build-scripts/test/test-py.hxml $end"
