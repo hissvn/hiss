3c0cd10e44d2e61f20c9dc9014ded1082a7751fa
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-10 14:00:27 -0600

    Refactor how test scripts run sequentially

--- test-all.sh
+++ test-all.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
-haxe build-scripts/test/test-interp.hxml
-haxe build-scripts/test/test-py.hxml
-haxe build-scripts/test/test-js.hxml
-haxe build-scripts/test/test-nodejs.hxml
-haxe build-scripts/test/test-cpp.hxml
+# If the first argument is supplied, test-all.sh will fail as soon as one target fails.
+end=""
+if [ ! -z "$1" ]; then
+    end="&& \\"
+fi
+
+# Build repls for each target that has one, so the tests can pipe input to them
+eval "haxe build-scripts/repl/build-cpp-repl.hxml $end
+haxe build-scripts/repl/build-nodejs-repl.hxml $end
+haxe build-scripts/repl/build-py-repl.hxml $end
+
+haxe build-scripts/test/test-interp.hxml $end
+haxe build-scripts/test/test-py.hxml $end
+haxe build-scripts/test/test-js.hxml $end
+haxe build-scripts/test/test-nodejs.hxml $end
+haxe build-scripts/test/test-cpp.hxml"
--- test-travis.sh
+++ test-travis.sh
@@ -6,8 +6,6 @@ haxelib install uuid
 haxelib install utest
 haxelib install hxnodejs
 haxelib install hxcpp
-haxe build-scripts/test/test-interp.hxml && \
-haxe build-scripts/test/test-py3.hxml && \
-haxe build-scripts/test/test-js.hxml && \
-haxe build-scripts/test/test-nodejs.hxml && \
-haxe build-scripts/test/test-cpp.hxml
+
+# For travis builds, use && to fail as soon as any target fails a test
+./test-all.sh yes
