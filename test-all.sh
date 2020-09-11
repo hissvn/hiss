44d87291df183e1027556c9721a8e835c368f06d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-11 14:53:00 -0600

    This should really fix it

--- test-all.sh
+++ test-all.sh
@@ -2,19 +2,21 @@
 
 # If the first argument is supplied, test-all.sh will fail as soon as one target fails.
 end=""
+pysuffix=""
 if [ ! -z "$1" ]; then
     end="&& \\"
+    pysuffix="3"
 fi
 
 eval "haxe build-scripts/test/test-interp.hxml $end
 
-haxe build-scripts/test/test-js.hxml $end
+haxe build-scripts/repl/build-py-repl.hxml && \
+haxe build-scripts/test/test-py$pysuffix.hxml $end
 
-haxe build-scripts/repl/build-cpp-repl.hxml && \
-haxe build-scripts/test/test-cpp.hxml $end
+haxe build-scripts/test/test-js.hxml $end
 
 haxe build-scripts/repl/build-nodejs-repl.hxml && \
 haxe build-scripts/test/test-nodejs.hxml $end
 
-haxe build-scripts/repl/build-py-repl.hxml && \
-haxe build-scripts/test/test-py.hxml"
+haxe build-scripts/repl/build-cpp-repl.hxml && \
+haxe build-scripts/test/test-cpp.hxml"
