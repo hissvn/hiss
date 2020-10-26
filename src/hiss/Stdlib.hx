ae2f468871453f3387e8cb29c329cee1b50971c1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-10-26 13:20:55 -0600

    fix type unification of listOrString

--- src/hiss/Stdlib.hx
+++ src/hiss/Stdlib.hx
@@ -478,7 +478,7 @@ class Stdlib {
     }
 
     /** Convert a Hiss index that could be negative, to a Haxe-friendly index >= 0. **/
-    public static function index(idx:Int, listOrString) {
+    public static function index(idx:Int, listOrString:{var length(default, null):Int;}) {
         var realIdx = idx;
         if (idx < 0)
             realIdx = listOrString.length + idx;
