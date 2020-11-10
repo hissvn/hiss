741fa341897716db4afc3cbe35e7f38b0b39d246
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-11-09 19:35:14 -0700

    Hello, world!

--- src/hiss/Hiss.hx
+++ src/hiss/Hiss.hx
@@ -9,22 +9,57 @@ class Hiss {
 	/**
 		Build a Haxe class from a corresponding .hiss file
 	**/
-	public static function build(hissFile) {
+	macro static public function build(hissFile:String):Array<Field> {
 		var classFields = Context.getBuildFields();
 
 		var stream = new Stream(hissFile);
 		var reader = new Reader();
 		while (!stream.isEmpty()) {
-			classFields.push(expressionToField(reader.read(stream)));
+			var position = stream.position();
+			var nextExp = reader.read(stream);
+			trace(nextExp);
+			// The last expression might be a comment, in which case None will be returned
+			switch (nextExp) {
+				case Some(nextExp):
+					classFields.push(readerExpToField(nextExp, position));
+				case None:
+			}
 		}
 
 		return classFields;
 	}
 
-	static function expressionToField(exp:ReaderExp):Field {
-		return {
-			// TODO make the pos be in the .hiss file!
-			pos: Context.currentPos();
+	static function readerExpToField(exp:ReaderExp, position:String):Field {
+		switch (exp) {
+			case Call(Symbol("defvar"), args) if (args.length == 2):
+				return {
+					name: switch (args[0]) {
+						case Symbol(name):
+							name;
+						default:
+							throw 'The first argument to defvar at $position should be a variable name';
+					},
+					access: [APublic, AStatic],
+					kind: FVar(null, // TODO allow type anotations
+						readerExpToHaxeExpr(args[1])),
+					pos: Context.currentPos()
+				};
+			default:
+				throw '$exp at $position is not a valid defvar or defun expression';
+		}
+	}
+
+	static function readerExpToHaxeExpr(exp:ReaderExp):Expr {
+		return switch (exp) {
+			case Symbol(name):
+				Context.parse(name, Context.currentPos());
+			case Str(s):
+				return {
+					pos: Context.currentPos(),
+					expr: EConst(CString(s))
+				};
+			default:
+				throw 'cannot convert $exp yet';
 		};
 	}
 }
--- src/hiss/Main.hx
+++ src/hiss/Main.hx
@@ -3,6 +3,6 @@ package hiss;
 @:build(hiss.Hiss.build("src/hiss/Main.hiss"))
 class Main {
 	public static function main() {
-		trace("Hello, compiled Hiss!");
+		trace(message);
 	}
 }
